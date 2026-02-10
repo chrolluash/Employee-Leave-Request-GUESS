b3e3c3bec6605351bdbcd4daf3e13613e4415233
doffy <165644698+chrolluash@users.noreply.github.com>
2026-02-10 14:22:54 +0800

    Rename AdminSeeder class to DatabaseSeeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
-class AdminSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
